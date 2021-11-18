06f464291e4973a01fdc35502ad1a1d405f01234
mariapaz <mpazcarvacho@gmail.com>
2021-11-18 13:16:06 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :tags
+  resources :posts
+
+  resources :post_tags
+
+  root to: 'posts#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
